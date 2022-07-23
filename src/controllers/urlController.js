683e0775e6baaae772c7288c3886c1805a29ca06
akthinq1 <akthinq@gmail.com>
2022-07-23 20:03:34 +0530

    Project4

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -1,5 +1,5 @@
 const urlModel = require('../models/urlModel')
-const mongoose = require("mongoose")
+//const mongoose = require("mongoose")
 const shortid = require('shortid')
 const validUrl = require('valid-url')
 const redis = require("redis");
